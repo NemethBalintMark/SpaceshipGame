85142da8360d340c77d36cdecebd7a643ae51909
NemethBalintMark <nemeth.balint.mark.2022i@bankitatabanya.hu>
2025-10-18 09:54:37 +0200

    script.js update

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ var gameArea = document.querySelector("section");
 var gameInterval = null;
 var score = 0;
 var scoreSpan = document.querySelector(".scoredisplay");
+var scoreInterval = null;
+
 
 document.querySelector(".startButton").addEventListener("click", () => {
-    score = 0;
-
     if (!gameInterval) {
-        gameInterval = setInterval( () => {
+        score = 0;
+        updateScore(0);
+        scoreInterval = setInterval(() => {
+            updateScore(1);
+        }, 1000);
+
+        document.createElement("div");
+        
+        gameInterval = setInterval(() => {
             var x = (Math.floor(Math.random() * 12) + 1) * 50;
             var enemy = new Enemy(x, 0, "section");
             enemy.fall();
@@ -15,12 +23,20 @@ document.querySelector(".startButton").addEventListener("click", () => {
     }
 })
 
+document.querySelector(".stopButton").addEventListener("click", () => {
+    lose();
+})
+
 function lose() {
     clearInterval(gameInterval);
     gameInterval = null;
+    clearInterval(scoreInterval);
+    scoreInterval = null;
 
     document.querySelectorAll(".weak").forEach(weak => weak.remove());
     document.querySelectorAll(".strong").forEach(strong => strong.remove());
+
+    alert("Game over!");
 }
 
 function updateScore(newScore) {
