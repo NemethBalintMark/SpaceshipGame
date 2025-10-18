eea851cc312435f5aad7e181d4be7bb4429e992a
NemethBalintMark <nemeth.balint.mark.2022i@bankitatabanya.hu>
2025-10-18 10:00:50 +0200

    player show

--- js/script.js
+++ js/script.js
@@ -14,9 +14,10 @@ document.querySelector(".startButton").addEventListener("click", () => {
         }, 1000);
 
         var player = document.createElement("div");
+        gameArea.appendChild(player);
         player.classList.add("player");
         player.style.left = 300 + "px";
-        player.style.top = 700 + "px";
+        player.style.top = 600 + "px";
 
         gameInterval = setInterval(() => {
             var x = (Math.floor(Math.random() * 12) + 1) * 50;
@@ -39,7 +40,7 @@ function lose() {
     document.querySelectorAll(".weak").forEach(weak => weak.remove());
     document.querySelectorAll(".strong").forEach(strong => strong.remove());
 
-    alert("Game over!");
+   // alert("Game over!");
 }
 
 function updateScore(newScore) {
